4f3cde8d41619998f41860143812b64b0c943558
Tyler Christensen <tyler9xp@gmail.com>
2016-11-03 00:08:47 -0600

    Add a timeout to the graceful shutdown example, since that's probably best practice

--- draining/drain.go
+++ draining/drain.go
@@ -34,7 +34,9 @@ func main() {
 	<-stopChan // wait for SIGINT
 	log.Println("Shutting down server...")
 
-	srv.Shutdown(context.Background())
+	// shut down gracefully, but wait no longer than 5 seconds before halting
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	srv.Shutdown(ctx)
 
 	log.Println("gracefully stopped stopped")
 
